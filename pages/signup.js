a8ff36eaababbbf3175594a51da4c13c6d66a6c7
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-10 16:50:00 +0200

    add basic functionality to the signup form

--- pages/signup.js
+++ pages/signup.js
@@ -1,71 +1,146 @@
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Snackbar from "@material-ui/core/Snackbar";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogActions from "@material-ui/core/DialogActions";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import DialogContentText from "@material-ui/core/DialogContentText";
-// import DialogTitle from "@material-ui/core/DialogTitle";
-// import Slide from "@material-ui/core/Slide";
-// import Gavel from "@material-ui/icons/Gavel";
-// import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import Gavel from "@material-ui/icons/Gavel";
+import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-class Signup extends React.Component {
-  state = {};
+import { signupUser } from "../lib/auth";
 
-  render() {
-    return <div>Signup</div>;
-  }
+class Signup extends React.Component {
+	state = {
+		name: "",
+		email: "",
+		password: ""
+	};
+
+	handleChange = e => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		const { name, email, password } = this.state;
+		const user = {
+			name,
+			email,
+			password
+		};
+		signupUser(user);
+	};
+
+	render() {
+		const { name, email, password } = this.state;
+		const { classes } = this.props;
+		return (
+			<div className={classes.root}>
+				<Paper className={classes.paper}>
+					<Avatar className={classes.avatar}>
+						<Gavel />
+					</Avatar>
+					<Typography variant="h5" component="h1">
+						Sign Up
+					</Typography>
+					<form onSubmit={this.handleSubmit} className={classes.form}>
+						<FormControl margin="normal" required fullWidth>
+							<InputLabel htmlFor="name">Name</InputLabel>
+							<Input
+								name="name"
+								type="text"
+								onChange={this.handleChange}
+								value={name}
+							/>
+						</FormControl>
+						<FormControl margin="normal" required fullWidth>
+							<InputLabel htmlFor="name">Email</InputLabel>
+							<Input
+								name="email"
+								type="email"
+								onChange={this.handleChange}
+								value={email}
+							/>
+						</FormControl>
+						<FormControl margin="normal" required fullWidth>
+							<InputLabel htmlFor="name">Password</InputLabel>
+							<Input
+								name="password"
+								type="password"
+								onChange={this.handleChange}
+								value={password}
+							/>
+						</FormControl>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							className={classes.submit}
+						>
+							Sign up
+						</Button>
+					</form>
+				</Paper>
+			</div>
+		);
+	}
 }
 
 const styles = theme => ({
-  root: {
-    width: "auto",
-    display: "block",
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up("md")]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto"
-    }
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: theme.spacing.unit * 2
-  },
-  signinLink: {
-    textDecoration: "none",
-    color: "white"
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing.unit
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 2
-  },
-  snack: {
-    color: theme.palette.secondary.light
-  },
-  icon: {
-    padding: "0px 2px 2px 0px",
-    verticalAlign: "middle",
-    color: "green"
-  }
+	root: {
+		width: "auto",
+		display: "block",
+		marginLeft: theme.spacing.unit * 3,
+		marginRight: theme.spacing.unit * 3,
+		[theme.breakpoints.up("md")]: {
+			width: 400,
+			marginLeft: "auto",
+			marginRight: "auto"
+		}
+	},
+	paper: {
+		marginTop: theme.spacing.unit * 8,
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		padding: theme.spacing.unit * 2
+	},
+	signinLink: {
+		textDecoration: "none",
+		color: "white"
+	},
+	avatar: {
+		margin: theme.spacing.unit,
+		backgroundColor: theme.palette.secondary.main
+	},
+	form: {
+		width: "100%",
+		marginTop: theme.spacing.unit
+	},
+	submit: {
+		marginTop: theme.spacing.unit * 2
+	},
+	snack: {
+		color: theme.palette.secondary.light
+	},
+	icon: {
+		padding: "0px 2px 2px 0px",
+		verticalAlign: "middle",
+		color: "green"
+	}
 });
 
 export default withStyles(styles)(Signup);
